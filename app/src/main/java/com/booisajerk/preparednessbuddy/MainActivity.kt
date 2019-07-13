1064d792ef66d7701392afcea61e7fe4b0ba8020
jennifer <jennifer@geocaching.com>
2019-07-13 16:04:31 -0700

    Add carousel

--- app/src/main/java/com/booisajerk/preparednessbuddy/MainActivity.kt
+++ app/src/main/java/com/booisajerk/preparednessbuddy/MainActivity.kt
@@ -3,10 +3,13 @@ package com.booisajerk.preparednessbuddy
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import com.google.android.material.bottomnavigation.BottomNavigationView
-import androidx.appcompat.app.AppCompatActivity
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.synnapps.carouselview.ImageListener
+import kotlinx.android.synthetic.main.carousel_view.*
 import kotlinx.android.synthetic.main.course_scroll_view.*
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -29,6 +32,11 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
+    //TODO add real images here
+    var sampleImages =
+        intArrayOf(R.drawable.ic_business_time, R.drawable.ic_business_time)
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -42,5 +50,14 @@ class MainActivity : AppCompatActivity() {
             startActivity(Intent(this, FormInputActivity::class.java))
             Log.d("Parker", "Open intent")
         }
+
+        // Carousel View
+        carouselView.pageCount = sampleImages.size
+        carouselView.setImageListener(imageListener)
+    }
+
+    var imageListener: ImageListener = ImageListener {
+            position, imageView ->
+        imageView.setImageResource(sampleImages[position])
     }
 }
