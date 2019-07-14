4ab2947e7855ac447da84e3149c6d2b820264f1a
jennifer <jennifer@geocaching.com>
2019-07-13 20:35:12 -0700

    Account setup activity

---
+++ app/src/main/java/com/booisajerk/preparednessbuddy/AccountSetupActivity.kt
@@ -0,0 +1,19 @@
+package com.booisajerk.preparednessbuddy
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_account_setup.*
+
+class AccountSetupActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_account_setup)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        household_task.setOnClickListener {
+            startActivity(Intent(this, HouseholdList::class.java))
+        }
+    }
+}
--- app/src/main/java/com/booisajerk/preparednessbuddy/MainActivity.kt
+++ app/src/main/java/com/booisajerk/preparednessbuddy/MainActivity.kt
@@ -2,13 +2,13 @@ package com.booisajerk.preparednessbuddy
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.synnapps.carouselview.ImageListener
 import kotlinx.android.synthetic.main.carousel_view.*
 import kotlinx.android.synthetic.main.course_scroll_view.*
+import kotlinx.android.synthetic.main.progress_view.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -41,7 +41,6 @@ class MainActivity : AppCompatActivity() {
     var sampleImages =
         intArrayOf(R.drawable.ic_chat_bubble_black_24dp, R.drawable.ic_mood_black_24dp)
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -49,11 +48,12 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
 
+        progress_button.setOnClickListener {
+            startActivity(Intent(this, AccountSetupActivity::class.java))
+        }
 
         scroll_button_one.setOnClickListener {
             //TODO for testing - remove
-            startActivity(Intent(this, FormInputActivity::class.java))
-            Log.d("Parker", "Open intent")
         }
 
         // Carousel View
